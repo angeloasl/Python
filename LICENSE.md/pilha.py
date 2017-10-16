0d10ede1fc7338c350a273f119da8e490398ffc8
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2017-10-16 09:40:50 -0200

    Update and rename LICENSE.md to pilha.py

---
+++ LICENSE.md/pilha.py
@@ -0,0 +1,11 @@
+pilha = ['A','N','G','E','L','O']
+>>> print("A pilha atual tem esses elementos:", end="")
+print(pilha)
+print("")
+for i in range(len(pilha)):
+print(pilha.pop(-1))
+
+print("")
+print("a A pilha atual tem esses elemntos:", end="")
+print(pilha)
+print("")
