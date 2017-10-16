760b96c5e37e0c48ffce47c8a2eef8be00475a12
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2017-10-16 09:41:58 -0200

    Create lista.py

---
+++ LICENSE.md/lista.py
@@ -0,0 +1,11 @@
+fila = ['A','N','G','E','L','O']
+>>> print("A pilha atual tem esses elementos:", end="")
+print(fila)
+print("")
+for i in range(len(pilha)):
+print(fila.pop(0))
+
+print("")
+print("a A pilha atual tem esses elemntos:", end="")
+print(fila)
+print("")
