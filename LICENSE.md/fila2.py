49cd08be6ac485ace41c1ca0320e6affaad1736d
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2017-10-16 09:58:32 -0200

    Create fila2.py

---
+++ LICENSE.md/fila2.py
@@ -0,0 +1,11 @@
+fila = ['A','N','G','E','L','O']
+universidade = ['U','F','S','C']
+>>> print("A pilha atual tem esses elementos:", end="")
+print(fila)
+print("")
+for i in range(len(fila)):
+	lista.insert(2+i, universidade[i])
+print("")
+print("a A pilha atual tem esses elemntos:", end="")
+print(fila)
+print("")
