63a94edf1107d84f9a6d9142e2e62bc8c9b536ed
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2017-10-31 11:21:04 -0200

    Update cadarq.py

--- LICENSE.md/cadarq.py
+++ LICENSE.md/cadarq.py
@@ -2,10 +2,34 @@
 info = {'Nome': "", 'IngressosRecebidos': "", 'Fase': ""} 
 cadVendedores = []
 resposta = True
+def NameCaracteres(string)
+    while (len(string)<10):
+        Name = Name + ""
+        Name = Name[:10]
+        return Name
+def NameCaracteres(string)
+    while (len(string)<2):
+        NIng = NIng + ""
+        NIng = NIng[:2]
+        return NIng
+def NameCaracteres(string)
+    while (len(string)<2):
+        Fase = Fase + ""
+        Fase = Fase[:2]
+        return Fase
+def NameCaracteres(string)
+    while (len(string)<2):
+        Age = Age + ""
+        Age = Age[:2]
+        return Age
+    
+    
 while resposta == True: 
     cadastro= open ("arq.txt","a")
     Name = raw_input("Qual o seu nome? ")
     cadastro.write(Name)
+    Age = raw_input("Voce tem quantos anos? ")
+    cadastro.write(Age)
     NIng = raw_input("Quantos ingressos voce recebeu? ")
     cadastro.write(NIng)
     Fase = raw_input("Voce esta em qual fase? ")  
@@ -20,4 +44,3 @@ while resposta == True:
         resposta=True
     else:
         resposta=False
-    
