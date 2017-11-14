eeeb3cadc013e16ec07af73182d245736ddddf2d
Angelo Lara <30839878+angeloasl@users.noreply.github.com>
2017-11-14 11:55:10 -0200

    Create prova.py

---
+++ LICENSE.md/prova.py
@@ -0,0 +1,38 @@
+print("Bem Vindo!")
+print("")
+estoque = open("estoque.txt", "r+")
+a = estoque.read(9);
+print "Produto1:", a
+b = estoque.read(3);
+print "Valor1:", b
+c = estoque.read(9);
+print "Produto2:", c
+d = estoque.read(3);
+print "Valor2:", d
+e = estoque.read(9);
+print "Produto3:", e
+f = estoque.read(3);
+print "Valor3:", f
+g = estoque.read(9);
+print "Produto4:", g
+h = estoque.read(3);
+print "Valor4:", h
+i = estoque.read(9);
+print "Produto5:", i
+j = estoque.read(3);
+print "Valor5:", j
+produto = [a,c,e,g,i]
+valor = [b,d,f,h,]
+usuario = int(input("Quantos produtos diferentes voce quer comprar?"))
+i = 1
+z = 0
+
+while(i <= usuario):
+    n=(input("Numero do Produto %i :" %i))
+    print(produto[n])
+    a=(input("Quantidade do Produto %i :" %i))
+    print(valor[n]*a)
+    i += 1
+
+
+    
